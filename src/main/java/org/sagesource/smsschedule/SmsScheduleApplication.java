b4e6747de9efb59d2ac9b496ea88d4296aff4236
sage.xue <sage.xue@vipshop.com>
2017-09-26 23:14:23 +0800

    init project

---
+++ src/main/java/org/sagesource/smsschedule/SmsScheduleApplication.java
@@ -0,0 +1,12 @@
+package org.sagesource.smsschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsScheduleApplication.class, args);
+	}
+}
